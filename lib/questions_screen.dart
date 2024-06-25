201f68b34f93168c4deeb5f25bef275ff707b9f6
Zeyad Nasrat <zeyad7nasrat@gmail.com>
2024-06-25 10:34:38 +0300

    first commit

---
+++ lib/questions_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build( context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        const Text('The question...'),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answer 1'),
+        ),
+        const Text('The question...'),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answer 2'),
+        ),
+        const Text('The question...'),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answer 3'),
+        ),
+      ],
+        ),
+    );
+ }
+}
